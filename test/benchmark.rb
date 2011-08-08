a860ae4cf1310c37981090f90b8afee06afe98a9
Duncan Robertson <duncan@whomwah.com>
2011-08-08 10:53:31 +0100

    update benchmark

--- test/benchmark.rb
+++ test/benchmark.rb
@@ -1,36 +1,57 @@
 require_relative '../lib/cbuffer.rb'
 require "benchmark"
 
-def pushing(ary,buf)
-  puts "Pushing onto a ruby array"
-  Benchmark.bm(7) do |x|
-    x.report("first:")   { (1..1000000).each { |i| ary.push(i) } }
+# Comparing a circular buffer implimentation for creating
+# a fixed size FIFO message queue to doing the same with 
+# an Array. As you will see, the circualr buffer approach
+# is much faster when it comes to writing.
+
+SAMPLE_SIZE=10000
+
+class ABuffer < Array
+  def initialize(size)
+    @ring_size = size
+    super( size )
   end
 
-  puts "Pushing onto a cbuffer"
-  Benchmark.bm(7) do |x|
-    x.report("first:")   { (1..1000000).each { |i| buf.put(i) } }
+  def put(element)
+    if length == @ring_size
+      shift
+    end
+    push element
+  end
+
+  def get(offset=0)
+    self[ - 1 + offset ]
   end
 end
 
-def pulling(ary,buf)
-  puts "Pull from a Array"
+def pushing(obj,txt)
+  puts txt 
   Benchmark.bm(7) do |x|
-    x.report("first:")   { (1..1000000).each { |i| ary.pop } }
+    x.report("first:")   { (1..SAMPLE_SIZE).each { |i| obj.put(i) } }
+    x.report("second:")  { (1..SAMPLE_SIZE).each { |i| obj.put(i) } }
+    x.report("third:")   { (1..SAMPLE_SIZE).each { |i| obj.put(i) } }
   end
+end
 
-  puts "Pull from an cbuffer"
+def pulling(obj,txt)
+  puts txt
   Benchmark.bm(7) do |x|
-    x.report("first:")   { (1..1000000).each { |i| buf.get } }
+    x.report("first:")   { (1..SAMPLE_SIZE).each { |i| obj.get } }
+    x.report("second:")  { (1..SAMPLE_SIZE).each { |i| obj.get } }
+    x.report("third:")   { (1..SAMPLE_SIZE).each { |i| obj.get } }
   end
 end
 
 def init
-  ary = Array.new(1000000)
-  buf = CBuffer.new(1000000)
+  ary = ABuffer.new(SAMPLE_SIZE)
+  buf = CBuffer.new(SAMPLE_SIZE)
 
-  pushing(ary,buf)
-  pulling(ary,buf)
+  pushing(ary,"Put: using Array Shift and Push")
+  pushing(buf,"Put: using CBuffer")
+  pulling(ary,"Get: using Array")
+  pulling(buf,"Get: using CBuffer")
 end
 
 init
