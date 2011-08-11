5bfff9afd2dba2422ebb579c98a55470ee3006d6
Duncan Robertson <duncan@whomwah.com>
2011-08-11 20:45:40 +0100

    add fix for Ruby 1.8

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,12 @@
 require 'test/unit'
 
+# fix for ruby 1.8
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require_relative '../lib/cbuffer.rb'
